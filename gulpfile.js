249db60dc8b15434d9b17d83d92425c01f013b68
tewcc1975 <tewcc1975@gmail.com>
2017-12-28 06:10:18 +0800

    Added Static File Monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var jsSources = [
     "components/scripts/template.js"
 ];
 var sassSources = ["components/sass/style.scss"];
+var htmlSources = ["builds/development/*.html"];
+var jsonSources = ["builds/development/js/*.json"];
 
 gulp.task("coffee", function() {
     gulp.src(coffeeSources)
@@ -47,6 +49,8 @@ gulp.task("watch", function() {
     gulp.watch(coffeeSources, ["coffee"]);
     gulp.watch(jsSources, ["js"]);
     gulp.watch("components/sass/*.scss", ["compass"]);
+    gulp.watch(htmlSources, ["html"]);
+    gulp.watch(jsonSources, ["json"]);
 });
 
 gulp.task("connect", function() {
@@ -56,4 +60,14 @@ gulp.task("connect", function() {
     });
 });
 
-gulp.task("default", ["coffee", "js", "compass", "connect", "watch"]);
+gulp.task("html", function() {
+    gulp.src(htmlSources)
+        .pipe(connect.reload());
+});
+
+gulp.task("json", function() {
+    gulp.src(jsonSources)
+        .pipe(connect.reload());
+});
+
+gulp.task("default", ["html", "coffee", "js", "compass", "connect", "watch"]);
